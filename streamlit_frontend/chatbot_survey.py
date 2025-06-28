5bd1b305940e229b102835efda2563a2f5728044
Lelox46 <145029508+Lelox46@users.noreply.github.com>
2025-06-28 15:25:27 +0200

    Create chatbot_survey.py

---
+++ streamlit_frontend/chatbot_survey.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title("📝 Fragebogen: Chatbot")
+
+st.markdown("""
+Bitte fülle den Fragebogen aus:
+
+👉 [Microsoft Forms Fragebogen zum Chatbot](https://forms.office.com/dein-link)
+
+Wenn du fertig bist, geht’s weiter:
+""")
+
+if st.session_state.gruppe == "chatbot_zuerst":
+    st.page_link("pages/2_Quizlet.py", label="Jetzt zur Quizlet-Lektion")
+else:
+    st.page_link("pages/5_Abschluss.py", label="Zur Abschluss-Seite")
