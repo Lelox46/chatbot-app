f0bb7bc4c6c9ffcf04217a72f8ab6dc0ef7af221
Lelox46 <145029508+Lelox46@users.noreply.github.com>
2025-06-28 15:26:36 +0200

    Create conclustion.py

---
+++ streamlit_frontend/conclustion.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("🎉 Danke fürs Mitmachen!")
+
+st.markdown("""
+Du hast beide Lernmethoden ausprobiert – danke dir!
+
+Wenn du möchtest, kannst du nun noch einen kurzen Abschlussfragebogen ausfüllen:
+
+👉 [Abschlussformular](https://forms.office.com/noch-ein-link)
+""")
