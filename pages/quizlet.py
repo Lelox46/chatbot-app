2d0dc02b869a11c70f047d14c09a91fa79b0de55
Lelox46 <145029508+Lelox46@users.noreply.github.com>
2025-06-30 22:08:25 +0200

    Update quizlet.py

--- pages/quizlet.py
+++ pages/quizlet.py
@@ -5,9 +5,11 @@ st.title("📘 Quizlet-Lektion")
 st.markdown("""
 Bitte mache nun die folgende Lektion auf Quizlet:
 
+Lerne mit den Karteikärtchen solange bis du ein gutes Gefühl für die Lernmethode hast. Keine Sorge, du musst dir nicht alles merken 😄.
+
 👉 [Hier geht's zur Lektion](https://quizlet.com/de/karteikarten/bitcoin-1040731195?i=wyc9j&x=1jqt)
 
-Wenn du fertig bist, klicke unten weiter.
+Wenn du genug hast, klicke bitte unten weiter.
 """)
 
 st.page_link("pages/survey_quizlet.py", label="Zum Fragebogen (Quizlet)")
