c90e0b55e055060742b3d7b6b203924ad430adee
Lelox46 <145029508+Lelox46@users.noreply.github.com>
2025-07-02 22:08:11 +0200

    Update conclusion.py

--- pages/conclusion.py
+++ pages/conclusion.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
-st.title("Noch eine letzte Sache...")
+st.title("Vielen Dank!")
 
 st.markdown("""
 Du hast beide Lernmethoden ausprobiert und bewertet– danke dir!
 
-Gleich hast du es geschafft😅.
-
-Bitte fülle noch diesen kurzen Abschlussfragebogen aus:
-
-👉 [Abschlussformular](https://forms.office.com/Pages/ResponsePage.aspx?id=oHqj8b2VxkW_mcmwY826GBQKxn3NUhFCm8LD9shjbppUNVpNUktQVzkxV0VJUE03TkpaQjcxVjBUMi4u)
 """)
+
+#Bitte fülle noch diesen kurzen Abschlussfragebogen aus:
+
+#👉 [Abschlussformular](https://forms.office.com/Pages/ResponsePage.aspx?id=oHqj8b2VxkW_mcmwY826GBQKxn3NUhFCm8LD9shjbppUNVpNUktQVzkxV0VJUE03TkpaQjcxVjBUMi4u)
